c0d2e62574e1e1eb4b5d280dc0d0194c2eb02c8c
dev3-nethermead <pratip@Pratips-LenovoG580>
2014-06-16 16:43:28 +0530

    assets compilation enabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,9 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-
+  
+  #Compress assets
+  config.assets.compress = true
   # Precompile additional assets.
   config.assets.precompile += %w( dashboard.js )
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
