3d67b20c3a0586f86ec1799fdcc56d1da99967f9
dev3-nethermead <pratip@Pratips-LenovoG580>
2014-06-16 16:22:44 +0530

    production.rb modified and assets.rb created for compilation of dashboard.js in Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ Rails.application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets.
+  config.assets.precompile += %w( dashboard.js )
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
