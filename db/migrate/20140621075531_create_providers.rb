40c3d8297d0910c381e236133da1b526c55b9285
dev3-nethermead <pratip@Pratips-LenovoG580>
2014-06-21 13:58:38 +0530

    modified migrations for provider and provider_charge according to data dictionary

---
+++ db/migrate/20140621075531_create_providers.rb
@@ -0,0 +1,27 @@
+class CreateProviders < ActiveRecord::Migration
+  def up
+    create_table :providers do |t|
+	
+	t.text :npi
+	t.text :npi_surrogate
+	t.text :first_name
+	t.text :org_last_name
+	t.text :designations, :limit => 50
+	t.string :img_url, :limit => 50
+	t.timestamps :physician_since
+	t.string :provider_type, :limit => 50
+	t.string :gender, :limit => 1
+	t.string :provider_address, :limit => 50
+	t.string :provider_suite_number, :limit => 50
+	t.string :provider_city
+	t.string :provider_zip
+	t.string :provider_county
+	t.string :provider_state
+	t.string :provider_country
+	t.string :provider_entity_type
+
+	t.timestamps
+    end
+    add_index :providers, :id, :primary_key, unique: true
+  end
+end
---
+++ db/migrate/20140621081356_create_provider_charges.rb
@@ -0,0 +1,46 @@
+class CreateProviderCharges < ActiveRecord::Migration
+  def up
+  	create_table :provider_charges do |t|
+      
+      t.text :service_claim_number
+      t.text :service_claim_line_number
+      t.integer :provider_id
+      t.integer :condition_procedure_id
+      t.integer :condition_procedure_type_id
+      t.integer :condition_icd_diagnosis_1_id
+      t.integer :condition_icd_diagnosis_2_id
+      t.integer :condition_icd_diagnosis_3_id
+      t.integer :condition_icd_diagnosis_4_id
+      t.integer :condition_icd_diagnosis_5_id
+      t.integer :condition_icd_diagnosis_6_id
+      t.integer :condition_icd_procedure_1_id
+      t.integer :condition_icd_procedure_2_id
+      t.integer :condition_icd_procedure_3_id
+      t.integer :condition_icd_procedure_4_id
+      t.integer :condition_icd_procedure_5_id
+      t.integer :condition_icd_procedure_6_id
+      t.integer :service_charge
+      t.integer :service_quantity
+      t.text :service_quantity_units
+      t.text :service_anesthesia_time
+      t.text :service_modifier_1
+      t.text :service_modifier_2
+      t.text :service_state
+      t.text :service_city
+      t.string :service_zip_code
+      t.text :service_specialty
+      t.text :service_network_type
+      t.string :service_year_from
+      t.string :service_year_to
+      t.text :service_place
+      t.text :service_type
+      t.text :service_plan_type
+      t.string :patient_id
+      t.string :patient_year_of_birth
+      t.string :patient_gender
+
+      t.timestamps
+    end
+    add_index :provider_charges, :id, unique: true
+  end
+end
