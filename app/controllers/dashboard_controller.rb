1d6dff6c08f5d55ef3960f0629eb14ee5a5fada5
dev3-nethermead <pratip@Pratips-LenovoG580>
2014-07-11 18:10:48 +0530

    change in algorithm for autocomplete

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,7 +12,7 @@ class DashboardController < ApplicationController
     if(searchProcedure == 'true')
       result = Condition.joins(
                "INNER JOIN procedures on conditions.code = procedures.code 
-                where conditions.consumer_name LIKE '%#{params[:q]}%' AND conditions.condition_type='procedure' order by conditions.id")
+                where conditions.consumer_name ILIKE '%#{params[:q]}%' AND conditions.condition_type='procedure' order by conditions.id")
                 .select("conditions.id,conditions.consumer_name, procedures.id AS p_id, procedures.short_name")
       result.each do |c|
         element = Hash.new
@@ -25,25 +25,42 @@ class DashboardController < ApplicationController
       end
     end
 
-    if(searchDiagnosis == 'true')
+if(searchDiagnosis == 'true')
       result = Condition.joins(
-               "INNER JOIN diagnosis on conditions.code = diagnosis.code 
-                where conditions.consumer_name LIKE '%#{params[:q]}%' AND conditions.condition_type='diagnosis' order by conditions.id")
-                .select("conditions.id,conditions.consumer_name, diagnosis.id AS d_id, diagnosis.short_name")
-
-      result.each do |d|
+               "INNER JOIN procedures on conditions.code = procedures.code 
+                where conditions.consumer_name ILIKE '%#{params[:q]}%' AND conditions.condition_type='diagnosis' order by conditions.id")
+                .select("conditions.id,conditions.consumer_name, procedures.id AS p_id, procedures.short_name")
+      result.each do |c|
         element = Hash.new
-        if d.consumer_name != ''
-          element[:category] = d.consumer_name
-          element[:name] =  d.short_name
-          element[:id] = d.d_id
+        if c.consumer_name != ''
+          element[:category] = "Diagnosis: " + c.consumer_name
+          element[:name] = c.short_name
+          element[:id] = c.p_id
           condition_array << element
-        end
+        end 
       end
     end
 
 
-    result = Procedure.where("short_name LIKE '%#{params[:q]}%'")
+    # if(searchDiagnosis == 'true')
+    #   result = Condition.joins(
+    #            "INNER JOIN diagnosis on conditions.code = diagnosis.code 
+    #             where conditions.consumer_name LIKE '%#{params[:q]}%' AND conditions.condition_type='diagnosis' order by conditions.id")
+    #             .select("conditions.id,conditions.consumer_name, diagnosis.id AS d_id, diagnosis.short_name")
+
+    #   result.each do |d|
+    #     element = Hash.new
+    #     if d.consumer_name != ''
+    #       element[:category] = d.consumer_name
+    #       element[:name] =  d.short_name
+    #       element[:id] = d.d_id
+    #       condition_array << element
+    #     end
+    #   end
+    # end
+
+
+    result = Procedure.where("short_name ILIKE '%#{params[:q]}%'")
     result.each do |p|
       element = Hash.new
       if p.full_name != ''
