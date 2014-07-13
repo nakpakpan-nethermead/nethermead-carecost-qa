98d5dd970f78d35db288837d1405e275d0c1f4f4
developer 3 <developer2@nethermead.com>
2014-07-13 15:31:18 +0530

    Query where fixed according to latest confirmation

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,6 +1,8 @@
 class Condition < ActiveRecord::Base
 	self.primary_key = 'id'
 
+  has_many :procedures
+  
 	scope :get_conditions, lambda{
 		select(:consumer_name, :codeset).all.map{|con| "#{con.consumer_name}|#{con.codeset}" }.join(',')
 	}
