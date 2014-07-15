123e995d591ddad8b089c806afefdd35f60efc85
developer 3 <developer2@nethermead.com>
2014-07-15 19:11:45 +0530

    Rechapta init file removed

--- config/initializers/recaptcha.rb
+++
@@ -1,5 +0,0 @@
-Recaptcha.configure do |config|
-  config.public_key  = '6Lc6BAAAAAAAAChqRbQZcn_yyyyyyyyyyyyyyyyy'
-  config.private_key = '6Lc6BAAAAAAAAKN3DRm6VA_xxxxxxxxxxxxxxxxx'
-  config.proxy = 'http://localhost:3000/'
-end
