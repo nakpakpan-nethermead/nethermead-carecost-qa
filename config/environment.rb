584bb815ae37f561432fddc1107ab74fad928b94
dev2 <nakpakpan@nethermead.com>
2014-07-18 13:32:21 -0500

    pull commit

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
    :address => "smtp.gmail.com",
    :port => 587,
-   :domain => "clear-cost.herokuapp.com",
+   :domain => "nethermead-carecost-qa.herokuapp.com",
    :authentication => :plain,
    enable_starttls_auto: true,
    :user_name => "herokuda@gmail.com",
